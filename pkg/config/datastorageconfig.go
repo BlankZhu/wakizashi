a59697a0a3fab43ffa8b756f72e555b5f7345a8f
BlankZhu <707310837@qq.com>
2020-12-22 22:19:33 +0800

    feat: update config for data backend

---
+++ pkg/config/datastorageconfig.go
@@ -0,0 +1,6 @@
+package config
+
+// DataBackendConfig is an interface for wakizashi's  data backends' config, like mysql, redis, influxdb...
+type DataBackendConfig interface {
+	// base struct, more todo
+}
