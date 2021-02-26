c5f2ca16d9eb00143837010e1454562f55e8825e
BlankZhu <707310837@qq.com>
2021-02-27 00:06:52 +0800

    update: optimization for performance

--- pkg/util/ip.go
+++ pkg/util/ip.go
@@ -6,29 +6,29 @@ import (
 )
 
 // GetIPSetFromNetworkInterface return a set of IP by given network interface
-func GetIPSetFromNetworkInterface(dev *net.Interface) map[string]bool {
-	ret := make(map[string]bool)
+func GetIPSetFromNetworkInterface(dev *net.Interface) map[string]struct{} {
+	ret := make(map[string]struct{})
 	addrs, err := dev.Addrs()
 	if err != nil {
 		return ret
 	}
 	for _, addr := range addrs {
-		ret[strings.Split(addr.String(), "/")[0]] = true
+		ret[strings.Split(addr.String(), "/")[0]] = struct{}{}
 	}
 	return ret
 }
 
 // GetIPSetFromNetworkInterfaces return a set of IP by given network interfaces,
 // duplicate IP will be overwrite
-func GetIPSetFromNetworkInterfaces(devs []net.Interface) map[string]bool {
-	ret := make(map[string]bool)
+func GetIPSetFromNetworkInterfaces(devs []net.Interface) map[string]struct{} {
+	ret := make(map[string]struct{})
 	for _, dev := range devs {
 		addrs, err := dev.Addrs()
 		if err != nil {
 			return ret
 		}
 		for _, addr := range addrs {
-			ret[strings.Split(addr.String(), "/")[0]] = true
+			ret[strings.Split(addr.String(), "/")[0]] = struct{}{}
 		}
 	}
 	return ret
